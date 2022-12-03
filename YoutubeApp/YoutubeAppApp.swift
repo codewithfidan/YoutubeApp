d42ecb76a446761443eac8e1a6fdd2a73ced5944
Fidan Oruc <fidanorucc>
2022-12-03 15:38:54 +0100

    Initial Commit

---
+++ YoutubeApp/YoutubeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoutubeAppApp.swift
+//  YoutubeApp
+//
+//  Created by Fidan Oruc on 03.12.22.
+//
+
+import SwiftUI
+
+@main
+struct YoutubeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
